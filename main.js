e7d04198f0f0f106e51df231ed36d040073f280d
NiharikaHari <hariniharika12@gmail.com>
2026-08-09 18:25:59 +0530

    done

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
 const lines = require('fs').readFileSync(0, 'utf8').split('\n');
-const METHODS = new Set(["GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"]);
+// 01 - Request Line
+// const METHODS = new Set(["GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"]);
 
-function isVersion(s) {
-  let matchResult = s.search(/^HTTP\/\d+\.\d+$/);
-  return matchResult !== -1;
-}
+// function isVersion(s) {
+//   let matchResult = s.search(/^HTTP\/\d+\.\d+$/);
+//   return matchResult !== -1;
+// }
 
+// for (const raw of lines) {
+//   const line = raw.replace(/\r$/, "");
+//   if (!line) continue;
+//   const parts = line.split(" ");
+//   if (parts.length !== 3 || !METHODS.has(parts[0]) || !parts[1].startsWith("/") || !isVersion(parts[2])) {
+//     console.log("INVALID");
+//     continue;
+//   }
+//   console.log(`METHOD=${parts[0]} PATH=${parts[1]} VERSION=${parts[2]}`);
+// }
+
+// 02 - Request Headers
 for (const raw of lines) {
   const line = raw.replace(/\r$/, "");
-  if (!line) continue;
-  const parts = line.split(" ");
-  if (parts.length !== 3 || !METHODS.has(parts[0]) || !parts[1].startsWith("/") || !isVersion(parts[2])) {
-    console.log("INVALID");
+  if (!line) break;
+  
+  if(!line.includes(":")) {
+    console.log(`ERR malformed: ${line}`);
     continue;
   }
-  console.log(`METHOD=${parts[0]} PATH=${parts[1]} VERSION=${parts[2]}`);
+
+  const idx = line.indexOf(":");
+  const name = line.slice(0, idx);
+  const value = line.slice(idx+1);
+
+  const formattedName = name.toLocaleLowerCase().trim();
+  const formattedValue = value.trim();
+
+  console.log(`${formattedName}: ${formattedValue}`);
+
 }
